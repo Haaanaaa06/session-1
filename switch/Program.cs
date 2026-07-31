b2d1bd4fd73fc0e3295ae5521f807db93d2300ee
hanahatem <hanahatem06@gmail.com>
2026-07-31 03:13:38 +0300

    Add files via upload

---
+++ switch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("enter a number from 1 to 7 ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please enter a number from 1 to 7.");
+                    break;
+            }
+        }
+    }
+}
