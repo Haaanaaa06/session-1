d493d7434d609341b378986238b87b7c62414b42
hanahatem <hanahatem06@gmail.com>
2026-07-31 03:05:18 +0300

    Add files via upload

---
+++ validORinvalid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp130
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 1 && num < 10)
+            { Console.WriteLine("Valid"); }
+            else
+            {
+                Console.WriteLine("invalid");
+
+            }
+        }
+    }
+}
